ee3c47ce77a2af8349e3f875a099593320e57ee1
Rima Pyne <rimapyne.rpt@gmail.com>
2026-01-28 08:32:41 +0530

    Initial commit

--- 01-folder/src/component/Nav.jsx
+++ 01-folder/src/component/Nav.jsx
@@ -5,14 +5,14 @@ import { FaSearch } from "react-icons/fa";
 
 function Nav() {
   return (
-    <div className="h-auto mt-2 pt-6 text-black flex justify-center gap-10 sm-gap-0 items-center shadow-lg ">
-                <div className='w-15 h-14 bg-white flex items-center rounded-xl justify-center shadow-lg'>
+    <div className="h-auto w-screen max-w-none px-4 mt-2 py-6 text-black bg-green-100 flex justify-center gap-10 items-center shadow-lg ">
+                <div className='w-15 sm:w-20 h-14 bg-white flex items-center rounded-xl justify-center shadow-lg'>
                 <MdFastfood className=" text-4xl text-green-500"/>
                 </div>
 
-                <form action="" className='h-14 px-20 flex items-center justify-center bg-white rounded-lg'>
-                    <FaSearch className='w-[20px] h-[20px] -px-3 flex justify-center text-green-500' />
-                    <input type="text" placeholder='Search' className='w-[50vw] h-10 rounded-lg p-2'/>
+                <form action="" className='h-14 flex items-center justify-center bg-white rounded-lg'>
+                    <FaSearch className='w-[20px] h-[20px]  flex justify-center text-green-500' />
+                    <input type="text" placeholder='Search' className='w-full outline-none h-10 rounded-lg '/>
                 </form>
 
                 <div className='w-15 h-14 sm-w-20 bg-white flex items-center rounded-xl justify-center shadow-lg'>
